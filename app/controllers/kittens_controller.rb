ce7ad21e5b8d253d3c0ec9300db4eae19f6faa7b
RunTheLi <lanmarock@gmail.com>
2025-05-31 12:51:45 +0700

    feat: add flash on kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -14,8 +14,10 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
+      flash[:notice] = "You added a cute kitten. Hooray"
       redirect_to @kitten
     else
+      flash.now[:alert] = "Oops! Something went wrong. Kitten aintn't valid"
       render :new
     end
   end
@@ -27,8 +29,10 @@ class KittensController < ApplicationController
   def update
     @kitten = Kitten.find(kitten_params)
     if @kitten.update
+      flash[:notice] = "You have Updated a cute lil Kitten, nice work"
       redirect_to @kitten
     else
+      flash.now[:alert] = "Update failed! Check your form, Kitten master"
       render :edit
     end
   end
@@ -36,6 +40,7 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
+    flash[:notice] = "Kitten deleted ... Why 😿"
     redirect_to kittens_path
   end
 
